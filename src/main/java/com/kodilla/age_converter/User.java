01ff128638c37e0494d06efe4aa7a519cb8db7d8
unknown <mateuszp.nowak@gmail.com>
2023-02-04 22:19:59 +0100

    Initial commit

---
+++ src/main/java/com/kodilla/age_converter/User.java
@@ -0,0 +1,2 @@
+package com.kodilla.age_converter;public class User {
+}
---
+++ src/test/java/com/kodilla/age_converter/AgeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.age_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgeConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
